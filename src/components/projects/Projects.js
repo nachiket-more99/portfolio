b04c8a0ee59c9ac9857cef7883938a30b155cb4a
Nachiket More <nachiketmore.more@gmail.com>
2025-06-08 16:07:21 -0700

    updated projects and github links

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -6,12 +6,25 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import { Typography } from "@mui/material";
 import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 
 const renderProjectCard = (item, index) => (
     <div className="project" key={index}>
         <Grid className="project-content">
             <Typography className="subheading">{item.date}</Typography>
-            <Typography className="heading font-bold">{item.project_title}</Typography>
+            <Typography className="heading font-bold" component="div">
+                {item.project_title}
+                {item.deployed_link && (
+                    <Link
+                        href={item.deployed_link}
+                        target="_blank"
+                        rel="noopener"
+                        style={{ marginLeft: '8px', verticalAlign: 'middle' }}
+                    >
+                        <OpenInNewIcon fontSize="small" />
+                    </Link>
+                )}
+            </Typography>
             <ul>
                 {
                     item.info.map((point, index) => (
@@ -23,19 +36,27 @@ const renderProjectCard = (item, index) => (
             </ul>
         </Grid>
         <Grid className="project-info">
-            <Stack project-tags direction="row" className="tags-container">
+            <Stack direction="row" className="tags-container">
                 {
                     item.tags.map((tag, index) => (
                         <Chip key={index} className="tag content" label={tag} />
                     ))
                 }
             </Stack>
-            <Link href={""} target="_blank">
-                <GitHubIcon className="social-icon" />
-            </Link>
+
+            {item.repo_link && (
+                <Link
+                    href={item.repo_link}
+                    target="_blank"
+                    rel="noopener"
+                >
+                    <GitHubIcon className="social-icon" />
+                </Link>
+            )}
         </Grid>
     </div>
 );
+
 
 const Projects = ({ projects }) => {
     return (
